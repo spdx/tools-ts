a56d038bfea1f5ef5b788d8cb3af11ec02a691f2
Anton Bauhofer <anton.bauhofer@tngtech.com>
2023-12-12 16:05:47 +0100

    Remove Rollup plugin
    The Rollup plugin was migrated to the following repository: https://github.com/spdx/rollup-plugin-spdx. Hence, we remove it from this repository.
    
    Signed-off-by: Anton Bauhofer <anton.bauhofer@tngtech.com>

--- rollup-plugin/examples/hello-world.ts
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2023 spdx contributors
-//
-// SPDX-License-Identifier: MIT
-
-export function helloWorld(): void {
-  console.log("hello world");
-}
--- rollup-plugin/examples/index.ts
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2023 spdx contributors
-//
-// SPDX-License-Identifier: MIT
-
-import { helloWorld } from "./hello-world";
-import { helloMars } from "./hello-mars";
-
-helloWorld();
-helloMars();
